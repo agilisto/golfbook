794972e23aa5ecd2c6292b66d36785ccf66c0bf1
armanddp <armanddp@c8967d08-c968-8642-90dd-964bea63534c>
2007-12-18 23:29:51 +0000

    fixed typo in mime_types.rb

--- Golfbook/config/initializers/mime_types.rb
+++ Golfbook/config/initializers/mime_types.rb
@@ -4,4 +4,4 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 
-Mine::Type.register "text/html", :facebook
+Mime::Type.register "text/html", :facebook
