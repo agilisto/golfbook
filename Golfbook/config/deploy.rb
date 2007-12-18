4a713f9a824f4c5f1b7ed1b4f9c0628277f4cad8
armanddp <armanddp@c8967d08-c968-8642-90dd-964bea63534c>
2007-12-18 23:09:54 +0000

    Updated to checkout correct version of our source

--- Golfbook/config/deploy.rb
+++ Golfbook/config/deploy.rb
@@ -2,7 +2,7 @@ require 'erb'
 require 'config/accelerator/accelerator_tasks'
 
 set :application, "Golfbook" #matches names used in smf_template.erb
-set :repository,  "svn://code.agilisto.com/golfbook/trunk"
+set :repository,  "svn://code.agilisto.com/golfbook/trunk/Golfbook"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
