a2e6deaa475708f78c81b0812fa40f09488edf23
armanddp <armanddp@c8967d08-c968-8642-90dd-964bea63534c>
2008-01-14 22:21:18 +0000

    Added flag shadows

--- Golfbook/app/controllers/map_controller.rb
+++ Golfbook/app/controllers/map_controller.rb
@@ -8,12 +8,17 @@ class MapController < ApplicationController
   def show   
     @course = Course.find(params[:id])
     @map = GMap.new(@size)
-    large_map = @size == 'large' ? true : false
+    
+    define_icons
+    
+    large_map = ((@size == 'large') || (@size == 'medium')) ? true : false
     @map.control_init(:large_map => large_map,:map_type => true)
     @map.center_zoom_init([@course.latitude, @course.longitude],@zoom.to_i)
 
+    info_window = render_to_string :partial => 'shared/info_window', :object => @course
+  
     marker = GMarker.new([@course.latitude, @course.longitude],   
-       :title => @course.name, :info_window => @course.description)  
+       :title => @course.name, :info_window => info_window, :icon => @icon_flag)  
      @map.overlay_init(marker)    
   end
   
@@ -31,10 +36,12 @@ class MapController < ApplicationController
     @map.control_init(:large_map => large_map,:map_type => true)
     @map.center_zoom_init([@user.latitude, @user.longitude], @zoom.to_i)
 
+    define_icons
+    
     @courses.each do |c|
       info_window = render_to_string :partial => 'shared/info_window', :object => c
       marker = GMarker.new([c.latitude, c.longitude],   
-         :title => c.name, :info_window => info_window)
+         :title => c.name, :info_window => info_window, :icon => @icon_flag)
       @map.overlay_init(marker)    
     end  
     rescue StandardError => e
@@ -56,6 +63,17 @@ class MapController < ApplicationController
   end
   
   private
+  def define_icons
+    # # Define the start and end icons  
+    @map.icon_global_init( GIcon.new( :image => url_for(:controller => :images, :action => 'red_flag_hole_icon.png'), 
+      :icon_size => GSize.new( 50,43 ), 
+      :icon_anchor => GPoint.new(25,20), 
+      :info_window_anchor => GPoint.new(39,15)),"icon_flag")
+      #:icon_shadow => url_for(:controller => :images, :action => 'shadow-red_flag_hole_icon.png'),
+      #:shadow_size => new GSize(72.0, 43.0)), 
+      
+    @icon_flag = Variable.new("icon_flag");
+  end
   def map_size
     @size = params[:size] ? params[:size] : 'large'
   end
