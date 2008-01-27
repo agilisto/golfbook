a174c0fb67081b58e94704bd541d1da58aae1201
armanddp <armanddp@c8967d08-c968-8642-90dd-964bea63534c>
2008-01-27 23:31:14 +0000

    Removed redundant migrations

--- Golfbook/db/migrate/012_wishlist_target_date.rb
+++
@@ -1,9 +0,0 @@
-class WishlistTargetDate < ActiveRecord::Migration
-  def self.up
-    add_column :wishlists, :target_date, :date
-  end
-
-  def self.down
-    remove_column :wishlists, :target_date
-  end
-end
