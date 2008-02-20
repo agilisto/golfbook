330a578fd4e5f73e6f206922ee315c6ec92e800e
craigp <craigp@c8967d08-c968-8642-90dd-964bea63534c>
2008-02-20 10:19:48 +0000

    fixed tour routes

--- Golfbook/config/routes.rb
+++ Golfbook/config/routes.rb
@@ -28,50 +28,56 @@ ActionController::Routing::Routes.draw do |map|
   
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => 'home'
+  
+  map.tour_edit "/tours/edit/:id", :controller => "tours", :action => "edit", :only_path => true
+  map.add_course_to_tour_by_location '/tours/:id/courses/searchbyloc', :controller => 'tours', :action => "search_for_course_by_name", :only_path => true
+  map.add_course_to_tour_by_name '/tours/:id/courses/searchbyname', :controller => 'tours', :action => "search_for_course_by_location", :only_path => true
+  map.invite_friends_to_tour "/tours/:id/users/new", :controller => "tours", :action => "addplayers", :only_path => true
+  map.tour_add_course "/tours/:id/courses/add/:course_id", :controller => "tours", :action => "add_course", :only_path => true
 
   map.course_add_round '/courses/:id/rounds/new', 
-      :controller => 'round', :action => 'new', 
-      :only_path => true
+    :controller => 'round', :action => 'new', 
+    :only_path => true
       
   map.user_view_rounds '/profile/:user_id/rounds/view', 
-      :controller => 'round', :action => 'index', 
-      :only_path => true
+    :controller => 'round', :action => 'index', 
+    :only_path => true
       
   map.course_have_played '/courses/:id/course_played/', 
-      :controller => 'courses', :action => 'course_played', 
-      :only_path => true
+    :controller => 'courses', :action => 'course_played', 
+    :only_path => true
   
   map.course_add_wishlist '/courses/:id/wishlist/new', 
-      :controller => 'wishlist', :action => 'new', 
-      :only_path => true
+    :controller => 'wishlist', :action => 'new', 
+    :only_path => true
   
   map.course_add_wishlist_target_date '/courses/:id/wishlist/target_date', 
-      :controller => 'wishlist', :action => 'set_target_date', 
-      :only_path => true
+    :controller => 'wishlist', :action => 'set_target_date', 
+    :only_path => true
   
   map.course_create_competition 'competitions/:id/new', 
-      :controller => 'competitions', :action => 'new', 
-      :only_path => true
+    :controller => 'competitions', :action => 'new', 
+    :only_path => true
 
   map.user_view_wishlist '/profile/:user_id/wishlist/view', 
-      :controller => 'wishlist', :action => 'index', 
-      :only_path => true
+    :controller => 'wishlist', :action => 'index', 
+    :only_path => true
       
   map.competition_select_course 'competitions/:id/select_course', 
-      :controller => 'competitions', :action => 'select_course', 
-      :only_path => true
+    :controller => 'competitions', :action => 'select_course', 
+    :only_path => true
 
   map.competition_course_selected 'competitions/:id/course_selected', 
-      :controller => 'competitions', :action => 'course_selected', 
-      :only_path => true
+    :controller => 'competitions', :action => 'course_selected', 
+    :only_path => true
 
   map.competition_edit 'competitions/:id/edit', 
-      :controller => 'competitions', :action => 'edit', 
-      :only_path => true
+    :controller => 'competitions', :action => 'edit', 
+    :only_path => true
   
   map.invite_players 'competitions/:id/invite', 
-      :controller => 'competitions', :action => 'invite_players', 
-      :only_path => true
+    :controller => 'competitions', :action => 'invite_players', 
+    :only_path => true
 
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
