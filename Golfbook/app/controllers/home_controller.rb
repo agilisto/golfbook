7945dbd497883e188640c7b8998ab27ad449e2e9
craigp <craigp@c8967d08-c968-8642-90dd-964bea63534c>
2008-02-20 10:37:41 +0000

    recent tours on home page

--- Golfbook/app/controllers/home_controller.rb
+++ Golfbook/app/controllers/home_controller.rb
@@ -8,6 +8,13 @@ class HomeController < ApplicationController
     @id = @user.id
     @action = NEAR_ME
     @recent_rounds = Round.find(:all, :order => 'created_at desc', :limit => 3)
+    
+    friends_uids = fbsession.friends_get.uid_list
+    users = User.find_all_by_facebook_uid friends_uids
+    uids = []
+    users.each { |u| uids << u.id }
+    @recent_tours = Tour.find_all_by_user_id uids, :order => :created_at, :limit => 3
+    
   end
   
 end
