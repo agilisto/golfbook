4478b9f65ac0d4df73ecfcde92f6f3bc95e1ef7f
armanddp <armanddp@c8967d08-c968-8642-90dd-964bea63534c>
2008-02-26 08:26:56 +0000

    Course ratings

--- Golfbook/app/controllers/rating_controller.rb
+++ Golfbook/app/controllers/rating_controller.rb
@@ -2,10 +2,15 @@ class RatingController < ApplicationController
 
   def rate
     @course = Course.find(params[:id])
+    
     Rating.delete_all(["rateable_type = 'Course' AND rateable_id = ? AND user_id = ?", 
       @course.id, current_user.id])
+    
     @course.add_rating Rating.new(:rating => params[:rating], 
       :user_id => current_user.id)
+    
+    request.format = :fbml  
+    render :partial => 'ratings/rate', :locals => { :asset => @course }
   end
 
 end
