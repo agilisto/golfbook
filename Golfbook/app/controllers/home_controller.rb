6b85c36f8fa940d8855e6f47d4533148c5951e92
armanddp <armanddp@c8967d08-c968-8642-90dd-964bea63534c>
2008-03-14 11:33:23 +0000

    Start of a changed layout on home

--- Golfbook/app/controllers/home_controller.rb
+++ Golfbook/app/controllers/home_controller.rb
@@ -7,21 +7,23 @@ class HomeController < ApplicationController
     @user = current_user
     @id = @user.id
     @action = NEAR_ME
-    @recent_rounds = Round.find(:all, :order => 'created_at desc', :limit => 3)
+    @recent_rounds = Round.find(:all, :order => 'rounds.created_at desc', :limit => 10, :include => [:course, :user])
     
     friends_uids = fbsession.friends_get.uid_list
     users = User.find_all_by_facebook_uid friends_uids
     uids = [ @user.id ]
     users.each { |u| uids << u.id }
+
     @recent_tours = Tour.find_all_by_user_id uids, :order => :created_at, :limit => 3
     @recent_competitions = Competition.find_all_by_user_id uids, :order => :created_at, :limit => 3
-    
+    @recent_friends_rounds = Round.find_all_by_user_id uids, :order => 'rounds.created_at desc', :limit => 10, :include => [:course, :user]
+        
     @upcoming_games = @user.games.upcoming
     @user.games_to_play.upcoming.each { |g| @upcoming_games << g }
     
     # causing "stack level too deep" exception
     #@recent_ratings = Course.find_last_rated 3
-    @recent_ratings = Rating.find :all, :order => "ratings.created_at desc", :limit => 3#, :include => [:user]
+    @recent_ratings = Rating.find :all, :order => "ratings.created_at desc", :limit => 10 #, :include => [:user]
   end
   
   def invite
