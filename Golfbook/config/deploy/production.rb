e155491b3580385f3640a32cea5f6d15e623c836
Ivor Paul <ivorpaul@gmail.com>
2009-02-20 15:55:54 +0200

    bugfixes, multistage setup

---
+++ Golfbook/config/deploy/production.rb
@@ -0,0 +1,28 @@
+set :application, "Golfbook" #matches names used in smf_template.erb
+set :repository,  "https://cap@code.agilisto.com:8443/svn/golfbook/trunk/Golfbook"
+
+set :deploy_to, "/home/993440e2/web/#{application}"
+
+# Replace XXXXXXXX with the username provided by Joyent in your welcome email.
+
+set :key_name, 'gnpcqaaa' 
+set :user, '993440e2'
+set :password, 'p3nqu1n'
+set :smf_process_user, 'root'
+set :smf_process_group, 'root'
+set :service_name, application
+set :working_directory, "#{deploy_to}/current"
+ssh_options[:paranoid] = false 
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :subversion
+set :domain, 'golfbook.agilisto.com'
+set :scm_password, "deploy"
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+set :server_name, key_name + ".joyent.us"
+set :server_alias, "*." + key_name + ".joyent.us"
---
+++ Golfbook/config/deploy/staging.rb
@@ -0,0 +1,53 @@
+set :application, "GolfbookDev" #matches names used in smf_template.erb
+
+set :working_directory, "#{deploy_to}/current"
+ssh_options[:paranoid] = false
+
+#
+# GitHub settings #######################################################################################
+default_run_options[:pty] = true
+set :repository, "git@github.com:agilisto/golfbook.git" #GitHub clone URL
+set :scm, "git"
+#set :scm_passphrase, "p3nqu1n" #This is the passphrase for the ssh key on the server deployed to
+set :branch, "master"
+set :scm_verbose, true
+set :deploy_via, :remote_cache
+#
+#########################################################################################################
+set :domain, 'golfbook.agilisto.com'
+
+# comment out if it gives you trouble. newest net/ssh needs this set.
+ssh_options[:paranoid] = false
+ssh_options[:forward_agent] = true
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+set :server_name, key_name + ".joyent.us"
+set :server_alias, "*." + key_name + ".joyent.us"
+
+#no need to restart apache
+deploy.task :restart do
+    accelerator.smf_restart
+#    accelerator.restart_apache
+end
+deploy.task :start do
+    accelerator.smf_start
+#    accelerator.restart_apache
+end
+deploy.task :stop do
+    accelerator.smf_stop
+#    accelerator.restart_apache
+end
+
+namespace :accelerator do
+task :create_vhost, :roles => :web do
+  puts "not creating vhost for staging."
+end
+
+task :tail_log, :roles => :app do
+    stream "tail -f #{shared_path}/log/staging.log"
+end
+
+after :deploy, 'deploy:cleanup'
