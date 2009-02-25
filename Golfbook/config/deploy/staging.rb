684dba34b315d9111231181e41dc18c2f2701a6f
Ivor Paul <ivorpaul@gmail.com>
2009-02-25 21:37:52 +0200

    repository_cache setting in capistrano

--- Golfbook/config/deploy/staging.rb
+++ Golfbook/config/deploy/staging.rb
@@ -20,9 +20,10 @@ set :smf_process_user, 'root'
 set :smf_process_group, 'root'
 
 # Settings to make app run from ./Golfbook ##############################################################
-_cset(:release_path)      { File.join(releases_path, release_name) }
-_cset(:current_release)   { File.join(releases_path, releases.last) }
-_cset(:previous_release)  { File.join(releases_path, releases[-2]) }
+#_cset(:release_path)      { File.join(releases_path, release_name) }
+#_cset(:current_release)   { File.join(releases_path, releases.last) }
+#_cset(:previous_release)  { File.join(releases_path, releases[-2]) }
+set :repository_cache, "cached-copy/Golfbook"   #this will hopefully make the repo copy to cacehd-copy but the copy process copy from cached-copy/Golfbook, alternatively it will make the repo deploy to shared/cached-copy/Golfbook leaving ./Golfbook from there...
 #########################################################################################################
 
 
