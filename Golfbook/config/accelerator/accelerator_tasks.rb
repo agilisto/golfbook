49c3c15ee98fde6c0692f418b0e9162e5ac781a3
Ivor Paul <ivorpaul@gmail.com>
2009-02-26 13:25:34 +0200

    staging as default mongrel cluster.yml

--- Golfbook/config/accelerator/accelerator_tasks.rb
+++ Golfbook/config/accelerator/accelerator_tasks.rb
@@ -24,6 +24,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
             start_port = cluster_info['port'].to_i
             puts "START PORT #{start_port}"
+            puts "end port should be - #{start_port + cluster_info['servers'].to_i - 1}"
             end_port = start_port + cluster_info['servers'].to_i - 1
             put "END PORT #{end_port}"
             public_path = "#{current_path}/public"
