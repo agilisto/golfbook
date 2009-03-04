92c395818853e0926135c127019083bb4d098105
Ivor Paul <ivorpaul@gmail.com>
2009-03-04 09:33:22 +0200

    changing some github settings

--- Golfbook/config/deploy/staging.rb
+++ Golfbook/config/deploy/staging.rb
@@ -13,6 +13,7 @@ set :scm_passphrase, "p3nqu1n" #This is the passphrase for the ssh key on the se
 set :branch, "master"
 set :scm_verbose, true
 set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
 #########################################################################################################
 set :domain, 'golfbook.agilisto.com'
 set :smf_process_user, 'root'
