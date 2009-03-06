71a1ae96a023a8d3f5e7fa6f36bef59b76907fd9
Ivor Paul <ivorpaul@gmail.com>
2009-03-06 10:31:36 +0200

    felt left out...

--- Golfbook/app/models/photo.rb
+++ Golfbook/app/models/photo.rb
@@ -6,4 +6,8 @@ class Photo < ActiveRecord::Base
     PhotoAsset.find(:all, :conditions => ['photo_id = ?',self.id]).collect{|x|x.asset}.flatten.compact.uniq
   end
 
+  def has_asset?(asset)
+    PhotoAsset.find(:first, :conditions => ['photo_id = ? AND asset_type = ? and asset_id = ?',self.id, asset.class.name, asset.id])
+  end
+
 end
